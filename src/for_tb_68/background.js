72bebce28e4775829758b475b88b48f64bb4f5d4
Takayama Fumihiko <tekezo@pqrs.org>
2019-07-06 23:42:58 +0900

    remove sample code (onToolbarClick)

--- src/for_tb_68/background.js
+++ src/for_tb_68/background.js
@@ -1,9 +1,1 @@
-// We defined this event in our schema.
-browser.myapi.onToolbarClick.addListener(function(toolbar, x, y) {
-  // We could do something interesting here with toolbar, x, and y, but we're not going to.
-
-  // We defined this function in our schema.
-  browser.myapi.sayHello('world');
-});
-
 browser.myapi.disableFolderTreeDrag();
