f37d9c54fd8d10758c13d0959b1fcbac6c1bb8b2
Takayama Fumihiko <tekezo@pqrs.org>
2025-03-16 22:15:07 +0900

    Remove tools

--- tools/get-version1.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-
-while l = gets
-  print Regexp.last_match(1) if /<em:version>(.+)<\/em:version>/ =~ l
-end
--- tools/get-version2.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby
-
-while l = gets
-  print Regexp.last_match(1) if /"version": "(.+)"/ =~ l
-end
